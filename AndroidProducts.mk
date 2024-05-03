f83a4bea583bee6d3edf4a67dde042882e56e8a7
lazycodebuilder <154446636+lazycodebuilder@users.noreply.github.com>
2024-05-03 23:52:38 +0530

    adding a decryption and Debugging

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,6 @@ PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_fogos.mk
 
 
 COMMON_LUNCH_CHOICES := \
-    twrp_fogos-userdebug
+    twrp_fogos-user \
+    twrp_fogos-userdebug \
+    twrp_fogos-eng
--- device.mk
+++ device.mk
@@ -72,6 +72,11 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
