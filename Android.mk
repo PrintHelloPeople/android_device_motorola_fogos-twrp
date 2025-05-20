a776b40f60e6c3cb11af03b5960c21e301517857
kay6888 <kaynikko88@gmail.com>
2025-05-20 18:16:44 -0500

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -1,13 +1,12 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),fogos)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+ifeq ($(TARGET_DEVICE),fogo)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
